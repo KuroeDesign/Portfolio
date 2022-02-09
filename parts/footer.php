3896246126709afa1afc9ceb2d42bec0f3c6264c
KuroeGame <96173258+KuroeDesign@users.noreply.github.com>
2022-02-09 10:08:51 +0100

    modif comment

--- parts/footer.php
+++ parts/footer.php
@@ -2,20 +2,20 @@
 <section class="bg-green text-white pt-5 pb-3">
     <div class="container text-center">
         <div class="row contacts mt-3 mb-3">
-            <div class="col-lg-3"></div>
+            <div class="col-lg-2"></div>
             <div class="col-xl-2">
                 <i class="fas fa-at fa-2x"></i>
                 <p><a href="mailto:cloe.ds@hotmail.com">cloe.ds@hotmail.com</a></p>
+            </div>
+            <div class="col-xl-4">
+                <i class="fas fa-map-marker-alt fa-2x"></i>
+                <p><a target="_blank" href="https://www.google.fr/maps/place/Rue+Guillaume+Appolinaire,+13500+Martigues/@43.4210413,5.0462761,18z/data=!3m1!4b1!4m5!3m4!1s0x12b61e3a8429a6d1:0xded466cb1607de10!8m2!3d43.4210394!4d5.0473704">Rue Guillaume Apollinaire, la Pastorale F<br/>13500 Martigues</a></p>
             </div>
             <div class="col-xl-2">
                 <i class="fas fa-phone-alt fa-2x"></i>
                 <p><a href="tel:+33634534359">06 34 53 43 59</a></p>
             </div>
-            <div class="col-xl-2">
-                <i class="fas fa-map-marker-alt fa-2x"></i>
-                <p><a target="_blank" href="https://www.google.fr/maps/place/Rue+Guillaume+Appolinaire,+13500+Martigues/@43.4210413,5.0462761,18z/data=!3m1!4b1!4m5!3m4!1s0x12b61e3a8429a6d1:0xded466cb1607de10!8m2!3d43.4210394!4d5.0473704">Rue Guillaume Apollinaire, la Pastorale F<br/>13500 Martigues</a></p>
-            </div>
-            <div class="col-md-3"></div>
+            <div class="col-md-2"></div>
         </div>
         <div class="row social mt-5 mb-5"> <!-- align 12 colonnes -->
             <div class="col-1"></div>
@@ -29,7 +29,7 @@
                 <a href="https://www.linkedin.com/in/clo%C3%A9-dufau-sansot-9b74361ba/" title="Linkedin" target="_blank"><i class="fab fa-linkedin-in fa-2x"></i></a>
             </div>
             <div class="col-2">
-                <a href="https://github.com/KuroeGame" title="GitHub" target="_blank"><i class="fab fa-github fa-2x"></i></a>
+                <a href="https://github.com/KuroeDesign" title="GitHub" target="_blank"><i class="fab fa-github fa-2x"></i></a>
             </div>
             <div class="col-2">
                 <a href="mailto: cloe-13500@hotmail.fr" title="Email" target="_blank"><i class="fas fa-envelope fa-2x"></i></a>
